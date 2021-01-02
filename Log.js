0303a51598b5127500e5d23bad4204c845cc1ba9
jinie-singh <anjani.1997mpvm@gmail.com>
2021-01-02 14:42:09 +0530

    platform added

--- Log.js
+++ Log.js
@@ -1,5 +1,6 @@
 class Log extends BaseClass{
   constructor(x,y,height,angle){
+    //something something
     super(x,y,20,height,angle);
     this.image = loadImage("sprites/wood2.png");
     Matter.Body.setAngle(this.body, angle);
